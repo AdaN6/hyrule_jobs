880a66a0b1b15b6e3fae1703bb00b7d595cec8b9
AdaN6 <a_disney_da@hotmail.com>
2023-03-27 15:18:13 +0200

    This is a new commit for what I originally planned to be amended

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
 <div class="app">
-  Hey ho
+  <!-- Practice A -->
+      <!-- <p>
+      {{ name }} - {{age}}
+      </p>
+      <button @click="changeName('Fleur')">Change Name</button>
+      <button @click="changeAge(30)">Change Age</button> -->
+  <!-- End of Practice A -->
+
+  <p>
+    {{ jobs[0].location }}
+  </p>
 </div>
 
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './types/Job';
 
 
 export default defineComponent({
   name: 'App',
   components: {
+  },
+  setup() {
+    // ------------- Practice A -------------
+      // ---> reactive
+      //   const state = reactive({
+        //     name: 'Link',
+      //     age: 25 as number | string, 
+      //   })
+
+      //   return { ...toRefs(state)}
+
+      // ---> ref
+
+      // const name = ref('Link')
+      // // generic argument for two different types of data
+      // const age = ref<number | string>(25)
+
+      // return { name,age}
+    // -------------End of Practice A -------------
+    const jobs = ref<Job[]>([
+      {title : 'farm worker', location: 'lon lon rance', salary: 3000, id: '1'},
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return { jobs }
+  },
+
+    // ------------- Practice A -------------
+
+      // data() {
+      //   return {
+      //     name: 'Link',
+      //     age: 25 as number | string,
+      //   }
+      // },
+    // -------------End of Practice A -------------
+  methods: {
+    
+    // ------------- Practice A -------------
+      // ----- for using data &
+        // changeName(name: string) {
+          //   this.name = name
+        //   return name
+
+        // },
+        // changeAge(age: number | string) {
+          //   this.age = age
+        //   return age
+        // }
+    // -------------End of Practice A -------------
+
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
