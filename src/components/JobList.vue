c07308ba5f89eb3879bdb92807a8840aa0459717
AdaN6 <a_disney_da@hotmail.com>
2023-03-27 16:26:34 +0200

    created prop in JobList component

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,12 +1,32 @@
 <template>
-    
+    <div class="job-list">
+        <ul>
+            <li v-for="job in jobs" :key="job.id">
+                <h2> {{job.title}} in {{job.location}} </h2>
+                <div class="salary">
+                    <p>
+                        {{job.salary}} pounds
+                    </p>
+                </div>
+                <div class="description">
+                    <p>this is just a mockup description. Please kindly take it as a template to see how it should be previewed. Thank you!</p>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
 
 export default defineComponent({
-   
+   props: {
+    jobs: {
+        required: true,
+        type: Array as PropType<Job[]>,
+    }
+   }
 })
 </script>
 
